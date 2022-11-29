a45933093dca19c213f5d00fd156eb44ee171d00
berk131 <berk131@gmail.com>
2022-11-29 17:55:22 +0500

    nvim without lsp

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,5 @@
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.nvim-tree"
+require "user.toggleterm"
---
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,21 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Nvimtree
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
